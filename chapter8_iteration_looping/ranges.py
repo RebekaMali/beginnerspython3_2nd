662f92cdaf5b196df563620866fcd20a58a42820
RebekaMali <rebeka.mali@etu.unige.ch>
2024-11-27 20:33:15 +0100

    added comments to example

--- chapter8_iteration_looping/ranges.py
+++ chapter8_iteration_looping/ranges.py
@@ -1,15 +1,16 @@
+# print all the numbers from 0 to 9
 for i in range(0, 10):
     print(i, ' ', end='')
 print()
 
+# print all the odd numbers from 1 to 9
 for i in range(1, 10, 2):
     print(i, ' ', end='')
 print()
 
+# print the even numbers starting from 10 
 for i in range(10, 1, -2):
     print(i, ' ', end='')
 print()
 
-for i in range(10):
-    print(i, ' ', end='')
-print()
+
