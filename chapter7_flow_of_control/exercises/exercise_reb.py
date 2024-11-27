be59b22e8ab8871314b69a61b1b8b122072205af
RebekaMali <rebeka.mali@etu.unige.ch>
2024-11-27 20:07:44 +0100

    did exercise chapter 7

---
+++ chapter7_flow_of_control/exercises/exercise_reb.py
@@ -0,0 +1,16 @@
+# Check if input is positive or negative
+user_input = int(input("Enter a number "))
+if user_input > 0:
+    print("the number is positive")
+else:
+    print("the number is negative")
+    
+
+# Test if a Number is Odd or Even
+num = int(input("Enter a valid number"))
+if num % 2 == 0:
+    print("The number is Even")
+else:
+    print("The number is Odd")
+    
+
